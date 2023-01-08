1ab736c1d43a0542343fac397653affc99f3238e
maharathi31 <hriday.rathi@plaksha.edu.in>
2023-01-08 16:38:13 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
 import './App.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import {Routes,Route} from 'react-router-dom'
+import {Actors,Movies,MovieInformation,Profile,Navbar} from "./components"
+import useStyles from "./styles"
 function App() {
+  const classes=useStyles();
   return (
     <div className="App">
-     Hi
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Movies/>}></Route>
+      <Route path="/actors/:id" element={<Actors/>}></Route>
+      <Route path="/movie/:id" element={<MovieInformation/>}></Route>
+      <Route path="/profile/:id" element={<Profile/>}></Route>
+    </Routes>
     </div>
   );
 }
